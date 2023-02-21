2808aeeb44221946fd43bab70e8026e17f937867
lkaijie <kaijie6@ualberta.ca>
2023-02-21 16:04:02 -0700

    new design

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         if (entry.isIntersecting) {
-//             entries.target.classList.add('show');
-//         } //else{
-//             //entry.target.classList.remove('show');
-//         //}
-//     });
-// });
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+// darkmode
+  // Function to toggle dark mode on and off
+  function toggleDarkMode() {
+    if (document.body.classList.contains("light-mode")) {
+      disableDarkMode();
+    } else {
+      enableDarkMode();
+    }
+}
 
+  // Function to enable dark mode
+function enableDarkMode() {
+    document.body.classList.add("light-mode");
+    localStorage.setItem("darkMode", "enabled");
+}
 
+  // Function to disable dark mode
+function disableDarkMode() {
+    document.body.classList.remove("light-mode");
+    localStorage.setItem("darkMode", "disabled");
+}
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((e1) => observer.observe(e1));
