5d1a5a8eca736d50fe8edd4cfb9a51f6530de81b
lkaijie <kaijie6@ualberta.ca>
2023-02-21 21:37:48 -0700

    closed a tag

--- app.js
+++ app.js
@@ -20,3 +20,19 @@ function disableDarkMode() {
     localStorage.setItem("darkMode", "disabled");
 }
 
+// animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((e1) => observer.observe(e1));
