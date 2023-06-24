0491fad9d3784cc2848f51e51da2b7916d4df785
lkaijie <kaijie6@ualberta.ca>
2023-06-24 01:10:31 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lkaijie.github.io/',
 
   server: {
     fs: {
