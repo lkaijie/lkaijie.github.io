267ee11b005802314b63a97aa2c9702ec5c3bf5f
lkaijie <94023052+lkaijie@users.noreply.github.com>
2025-10-06 00:04:27 -0600

    fix hidden img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -77,7 +77,7 @@ export default function About() {
               </ul>
             </div>
           </div>
-          <div className="about-img hidden sm:w-2/4 sm:ml-5">
+          <div className="about-img hidden lg:block lg:w-2/4 lg:ml-5">
             {/* <img src={about_img} alt="profile" /> */}
             <img src={about_img} alt="profile" />
             {/* both work */}
